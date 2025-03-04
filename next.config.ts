3f3e65b62682e35a7671f5bc979fa5dd9ed7d110
justgoodluck <403028240@qq.com>
2025-03-05 03:48:51 +0800

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,31 @@
-export default {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
-        search: ''
+        search: '',
       },
       {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
-        search: ''
-      }
-    ]
-  }
+        search: '',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // 如果是服务端构建，添加 node-loader 处理 .node 文件
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.node$/,
+        use: 'node-loader',
+      });
+    }
+
+    return config;
+  },
 };
+
+export default nextConfig;
